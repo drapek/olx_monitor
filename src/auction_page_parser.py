dd6341956947e853a015fcef896b01a3c1a391ee
Paweł Drapiewski <pawel.drapiewski@cloudware.pl>
2021-10-09 10:26:16 +0200

    Add support for otomoto pl

--- src/auction_page_parser.py
+++ src/auction_page_parser.py
@@ -1,6 +1,6 @@
-import abc
 import time
 from abc import ABC, abstractmethod
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
@@ -25,6 +25,8 @@ class AuctionPageParser:
                 parser = OlxParser()
             if 'sprzedajemy.pl' in url:
                 parser = SprzedajemyParser()
+            if 'otomoto.pl' in url:
+                parser = OtomotoParser()
             if parser is None:
                 log_print(f"[ERROR] couldn't find Page Parser for the site {url}")
                 return
@@ -81,7 +83,7 @@ class OlxParser(PageParser):
         return offer_data
 
 
-class SprzedajemyParser:
+class SprzedajemyParser(PageParser):
     def analyze_html_page(self, html_content):
         soup = BeautifulSoup(html_content, 'html.parser')
 
@@ -108,5 +110,33 @@ class SprzedajemyParser:
             'image_url': get_value_or_none("offer_html.find('img').attrs['src']", offer_html),
             'offer_url': 'https://sprzedajemy.pl' + get_value_or_none("offer_html.find('a').attrs['href']", offer_html),
             'data_id': offer_html.attrs['id'][len('offer-'):]  # strip the 'offer-' prefix
+        }
+        return offer_data
+
+
+class OtomotoParser(PageParser):
+    def analyze_html_page(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        # Get the first listing ul - these are normal offers and not outdated
+        offers_html = soup.find_all('div', class_='offers')[0].find_all('article')
+
+        offers_dict = {}
+        for offer_html in offers_html:
+            offer_data = self.analyze_offer(offer_html)
+            offer_id = f"otomoto_{offer_data['data_id']}"
+            offers_dict[offer_id] = offer_data
+
+        return offers_dict
+
+    def analyze_offer(self, offer_html):
+        offer_data = {
+            'title': get_value_or_none("offer_html.find('a', class_='offer-title__link').attrs['title']", offer_html),
+            'price': get_value_or_none("offer_html.find('span', class_='offer-price__number').text", offer_html),
+            'localization': get_value_or_none("offer_html.find('h4', class_='ds-location').text", offer_html),
+            'add_time': f'empty - but the offer was found at {datetime.now()}',
+            'image_url': get_value_or_none("offer_html.find('img').attrs['data-src']", offer_html),
+            'offer_url': 'https://sprzedajemy.pl' + get_value_or_none("offer_html.find('a').attrs['href']", offer_html),
+            'data_id': offer_html.attrs['data-ad-id']  # strip the 'offer-' prefix
         }
         return offer_data
