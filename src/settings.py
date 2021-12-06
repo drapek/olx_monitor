d5352cb365ef53a69ac02d005b066b5e55e809f6
Paweł Drapiewski <pawel.drapiewski@cloudware.pl>
2021-12-06 22:24:34 +0100

    Add NordVPN support and small dir structure refactor
    1. Add NordVPN support as standalone container that will be used as "gate"
    for the OLX Monitor container
    2. Reorganize the folders to project clearance and easier maintainability
    from the docker-compose perspective

--- src/settings.py
+++ src/settings.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 
 SCAN_INTERVAL_SEC = 60 * 10
 WORKING_HOURS = (8, 23)
-OUT_FILE = '../found_offers.json'
-DB_FILE = '../db.file'
+OUT_FILE = '../persistent/found_offers.json'
+DB_FILE = '../persistent/db.file'
 LOG_LEVEL = 3  # 3 - Debug, 2 - Normal, 1 - No output
 
 # Load data as environment variables from .envconfig file.
@@ -16,7 +16,7 @@ EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 MESSENGER_APP_TOKEN = os.getenv('MESSENGER_APP_TOKEN')
 
 # Load search data from YAML file
-with open('../searchconfig.yaml', 'r') as yaml_file:
+with open('../persistent/searchconfig.yaml', 'r') as yaml_file:
     yaml_data = yaml.load(yaml_file)
 
     OLX_SITES_TO_MONITOR = yaml_data['olx_urls']
